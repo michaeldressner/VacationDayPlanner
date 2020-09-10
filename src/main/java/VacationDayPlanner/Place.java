2dab15b56b1a13fe1feb275dd6b50d519b8c16b3
mdressne <mdressne@umd.edu>
2020-09-10 14:50:53 -0400

    finished Place class

--- src/main/java/VacationDayPlanner/Place.java
+++ src/main/java/VacationDayPlanner/Place.java
@@ -4,11 +4,27 @@ public class Place {
 	private String name;
 	private double lat;
 	private double lng;
+	private int numRatings;
+	private double rating;
+	private String vicinity;
 	
 	public Place(String name, double lat, double lng) {
 		this.name = name;
 		this.lat = lat;
 		this.lng = lng;
+		this.numRatings = -1;
+		this.rating = 0.0;
+		this.vicinity = "";
+	}
+	
+	public Place(String name, double lat, double lng, int numRatings,
+			double rating, String vicinity) {
+		this.name = name;
+		this.lat = lat;
+		this.lng = lng;
+		this.numRatings = numRatings;
+		this.rating = rating;
+		this.vicinity = vicinity;
 	}
 	
 	public String getName() { return name; }
@@ -16,4 +32,10 @@ public class Place {
 	public double getLat() { return lat; }
 	
 	public double getLng() { return lng; }
+	
+	public int getNumRatings() { return numRatings; }
+	
+	public double getRating() { return rating; }
+	
+	public String getVicinity() { return vicinity; }
 }
